51bf938ba23fcafd3e3de2be6ce8f7def3161c5d
xiongjian <webjsforyou@gmail.com>
2020-09-14 14:53:54 +0800

    feat: 完成城市选择器

--- src/components/CountryPicker/index.jsx
+++ src/components/CountryPicker/index.jsx
@@ -1,30 +1,81 @@
-import React, { Component } from "react";
-import { NavBar, Icon } from "antd-mobile";
+import React, { Component, Fragment } from "react";
+import { NavBar, Icon, List } from "antd-mobile";
 
 import { reqCountryData } from "@api/common";
+import "./index.css";
+
+const Item = List.Item;
 
 export default class CountryPicker extends Component {
+  state = {
+    countryData: {},
+  };
+
   componentDidMount() {
     // 发送请求请求数据
     reqCountryData()
       .then((res) => {
         console.log("成功~", res);
+        this.setState({
+          countryData: res,
+        });
       })
       .catch((err) => {
         console.log("失败~", err);
       });
   }
 
+  jumpToCountry = (e) => {
+    // console.log(e.target.textContent);
+    document.getElementById(e.target.textContent).scrollIntoView();
+  };
+
   render() {
+    const { countryData } = this.state;
+
+    const keys = Object.keys(countryData);
+
     return (
-      <div>
+      <div className="country">
         <NavBar
+          className="country-nav"
           mode="light"
           icon={<Icon className="icon-left" type="left" />}
           onLeftClick={() => console.log("onLeftClick")}
         >
           选择国家或者地区
         </NavBar>
+        <div className="country-navbar" onTouchEnd={this.jumpToCountry}>
+          {keys.map((key, index) => {
+            return <a key={index}>{key}</a>;
+          })}
+        </div>
+        <div className="country-container">
+          {keys.map((key, index) => {
+            const items = countryData[key];
+            return (
+              <div key={index} className="country-list">
+                <div className="country-first" id={key}></div>
+                <List renderHeader={() => key}>
+                  {items.map((item, index) => {
+                    // 注意item是对象
+                    const key = Object.keys(item)[0];
+                    const value = item[key];
+                    return (
+                      <Item
+                        className="country-item"
+                        key={index}
+                        extra={"+" + value}
+                      >
+                        {key}
+                      </Item>
+                    );
+                  })}
+                </List>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
